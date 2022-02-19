a90ad02d715bedb2586bdbdd10a195706c650853
julian garcia <juliancamilogarciaescobar7@gmail.com>
2022-02-19 02:34:44 -0500

    fixing

--- src/context/Api.js
+++ src/context/Api.js
@@ -1,5 +1,6 @@
 const server = "https://authentionappbackend.herokuapp.com/";
 const apiLogin = `${server}/api/login`;
+const serverUser = `${server}/user`;
 const apiRegister = `${server}/api/register`;
 
-module.exports = {server, apiLogin, apiRegister};
+module.exports = {server, apiLogin, apiRegister, serverUser};
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import { server } from '../context/Api';
 
 const User = () => {
+
+    const [ searchParams, setSearchParams ] = useSearchParams();
+    const cookies = new Cookies();
+    cookies.set('access_token', searchParams.get('access_token'), { path:'/'});
+    useEffect(() => {
+        fetch(`${server}`)
+    }, []);
     return (
 
     <div>
